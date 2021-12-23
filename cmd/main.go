92f238a961a9e182c526863aae9e75607912a683
A. Mogilevskiy <dark.qvark@gmail.com>
2021-12-23 11:28:09 +0300

    feat (core) add README.md

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
+	//http.ListenAndServe(":80", )
 	fmt.Println("RUN")
+
+	http.HandleFunc("/one", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("one"))
+	})
+
+	http.HandleFunc("/two", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("two"))
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
