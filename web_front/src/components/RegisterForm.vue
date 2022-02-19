4a3b11e71d1d67c5cd8f6ecc9bf47321f9756df3
dark.qvarkq <dark.qvark@gmail.com>
2022-02-19 14:33:58 +0300

    (feat) init RegisterForm.vue

---
+++ web_front/src/components/RegisterForm.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <form>
+    <input placeholder="azaza" type="text">
+  </form>
+</template>
+
+<style scoped>
+
+</style>
---
+++ web_front/src/views/WelcomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import RegisterForm from '@/components/RegisterForm.vue'
+</script>
+
+<template>
+  <RegisterForm />
+</template>
+
+<style scoped>
+</style>
