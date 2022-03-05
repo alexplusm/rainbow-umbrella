863499b349c5b05279b1cead63d5a9249d83bd4e
dark.qvarkq <dark.qvark@gmail.com>
2022-03-05 11:45:15 +0300

    (feat) rm unused code

--- web_front/src/components/Profile.vue
+++ web_front/src/components/Profile.vue
@@ -24,65 +24,6 @@ console.log("sessionId", sessionId)
 const userStore = useUserStore();
 userStore.retrieve(login as string);
 
-// const headers = new Headers();
-// if (sessionId != null) {
-//   headers.append('X-SessionId', sessionId)
-// }
-//
-// const user: UserM = await fetch(`/api/v1/users/${login}`, {headers})
-//   .then(resp => resp.json())
-//   .then(data => {
-//     console.log("data", data);
-//     const user = new UserM(data)
-//
-//     user.age = 666;
-//
-//     return user;
-//   });
-
-// console.log("USERRR: ", user);
-
-
-// export default {
-//   async setup() {
-//     const login = router.currentRoute.value.params['login'];
-//     console.log("curr login", login);
-//
-//     const sessionId = localStorage.getItem("X-SessionId");
-//
-//     console.log("sessionId", sessionId)
-//
-//     // withAsyncContext()
-//
-//     const headers = new Headers();
-//     if (sessionId != null) {
-//       headers.append('X-SessionId', sessionId)
-//     }
-//
-//     // const user: UserM = await fetch(`/api/v1/users/${login}`, {headers})
-//     //   .then(resp => resp.json())
-//     //   .then(data => {
-//     //     console.log("data", data);
-//     //     const user = new UserM(data)
-//     //
-//     //     user.age = 666;
-//     //
-//     //     return user;
-//     //   });
-//     //
-//     // console.log("USERRR: ", user);
-//
-//     onUnmounted(
-//         () => console.log('Unmounted'),
-//         // instance // <--- pass the instance to it
-//         // "wtf";
-//     )
-//
-//     return {
-//       // user
-//     }
-//   }
-// }
 </script>
 
 <style scoped>
