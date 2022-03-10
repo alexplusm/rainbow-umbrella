4ca52127c2d2a437b9eca7a466a33c0319b17ce6
dark.qvarkq <dark.qvark@gmail.com>
2022-03-10 12:59:56 +0300

    (feat) upd Profile.vue

--- web_front/src/components/Profile.vue
+++ web_front/src/components/Profile.vue
@@ -1,24 +1,62 @@
 <template>
   <section>
-    KEKW
-    {{ userStore.currentUser?.login }}
+    <q-card class="my-card" flat bordered>
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ currentUser.firstName }} {{ currentUser.lastName }}</q-item-label>
+          <q-item-label caption>
+            {{ currentUser.login }}
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator />
+
+      <q-card-section>
+        <ul>
+          <li>
+            Age: {{ currentUser.age }}
+          </li>
+          <li>
+            City: {{ currentUser.city}}
+          </li>
+          <li>
+            Gender: {{ currentUser.gender }}
+          </li>
+        </ul>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-section>
+        Your friend
+      </q-card-section>
+
+      <q-card-actions>
+        <q-btn flat>
+          Add to friend
+        </q-btn>
+        <q-btn flat color="primary">
+          Reserve
+        </q-btn>
+      </q-card-actions>
+    </q-card>
   </section>
 </template>
 
 <script lang="ts" setup>
-import router from '@/router'
 import {useUserStore} from "@/stores/user";
+import type {UserM} from "@/models/user";
 
 const userStore = useUserStore()
 
-const login = router.currentRoute.value.params['login'];
-console.log("curr login", login);
-
-const sessionId = localStorage.getItem("X-SessionId");
-
-console.log("sessionId", sessionId);
-
-userStore.retrieve(login as string);
+const currentUser: UserM = userStore.currentUser as UserM;
 </script>
 
 <style scoped>
