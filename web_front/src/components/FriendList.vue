9da9d155cf1d02c5fef269561c3d69e785b4f144
dark.qvarkq <dark.qvark@gmail.com>
2022-03-11 11:59:54 +0300

    (feat) upd FriendList.vue

--- web_front/src/components/FriendList.vue
+++ web_front/src/components/FriendList.vue
@@ -1,19 +1,29 @@
 <script setup lang="ts">
-import ProfileMini from "@/components/ProfileMini.vue"
-import {useUserStore} from "@/stores/user";
+import ProfileMini from "@/components/ProfileMini.vue";
+import { useUserStore } from "@/stores/user";
 
 const userStore = useUserStore();
-
-
 </script>
 
 <template>
   <section>
-    FriendList
+    <q-card flat bordered>
+      <q-card-section>
+        <div class="text-h6">Friend List</div>
+      </q-card-section>
 
-    <ProfileMini :user="userStore.currentUser" :show-actions="false"/>
+      <q-card-section>
+        <ProfileMini :user="userStore.currentUser" :show-actions="false"/>
+        <ProfileMini :user="userStore.currentUser" :show-actions="false"/>
+        <ProfileMini :user="userStore.currentUser" :show-actions="false"/>
+      </q-card-section>
+    </q-card>
   </section>
 </template>
 
 <style scoped>
+section {
+  display: flex;
+  flex-direction: column;
+}
 </style>
