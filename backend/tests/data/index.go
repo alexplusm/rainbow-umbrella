0a15d45e4afbe02f03946280841f18780bbc8be5
dark.qvarkq <dark.qvark@gmail.com>
2022-03-15 15:05:16 +0300

    (feat) upd generation scripts

--- backend/tests/data/index.go
+++ backend/tests/data/index.go
@@ -21,7 +21,7 @@ var (
 	}
 )
 
-const UserCount = 3
+const UserCount = 10
 
 func main() {
 	appConfig, err := new(infrastruct.AppConfig).BuildFromEnv()
@@ -66,7 +66,7 @@ func (g *userGenerator) build() (*userGenerator, error) {
 	g.hashedPassword = "$2a$10$F.QZ5yG1OVLeSjb.kNGKveVAt6oIT8IYi5bavieoel3A5PHDJlFNG"
 	g.loginPrefix = fmt.Sprintf("gen_login_%v", time.Now().Unix())
 
-	firstNamesRaw, err := ioutil.ReadFile("first_names.txt")
+	firstNamesRaw, err := ioutil.ReadFile("tests/data/first_names.txt")
 	if err != nil {
 		return nil, err
 	}
@@ -76,7 +76,7 @@ func (g *userGenerator) build() (*userGenerator, error) {
 		}
 	}
 
-	lastNamesRaw, err := ioutil.ReadFile("last_names.txt")
+	lastNamesRaw, err := ioutil.ReadFile("tests/data/last_names.txt")
 	if err != nil {
 		return nil, err
 	}
@@ -94,24 +94,32 @@ func (g *userGenerator) generateLogin() string {
 	return fmt.Sprintf("%v_%v", g.loginPrefix, g.loginCounter)
 }
 
-func (g *userGenerator) generateCity() string {
+func (g userGenerator) generateCity() string {
 	return Cities[rand.Intn(len(Cities))]
 }
 
-func (g *userGenerator) generateFirstName() string {
+func (g userGenerator) generateFirstName() string {
 	return g.firstNames[rand.Intn(len(g.firstNames))]
 }
 
-func (g *userGenerator) generateLastName() string {
+func (g userGenerator) generateLastName() string {
 	return g.lastNames[rand.Intn(len(g.lastNames))]
 }
 
-func (g *userGenerator) generateGender() string {
+func (g userGenerator) generateGender() string {
 	probability := rand.Float64()
 	if probability >= 0.51 {
 		return "Female"
 	}
 	return "Male"
+}
+
+func (g userGenerator) generateBirthdate() time.Time {
+	year := 1950 + rand.Intn(60)
+	month := time.Month(1 + rand.Intn(9))
+	day := 1 + rand.Intn(24)
+
+	return time.Date(year, month, day, 0, 0, 0, 0, time.Local)
 }
 
 func (g *userGenerator) generateUserDAO() *dao.User {
@@ -124,6 +132,7 @@ func (g *userGenerator) generateUserDAO() *dao.User {
 	userBO.FirstName = g.generateFirstName()
 	userBO.LastName = g.generateLastName()
 	userBO.Gender = g.generateGender()
+	userBO.Birthday = g.generateBirthdate()
 
 	return new(dao.User).FromBO(userBO)
 }
