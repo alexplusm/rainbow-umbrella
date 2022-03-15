ed56bd1ebb4e6fe06fab2589f61e429c6f258409
dark.qvarkq <dark.qvark@gmail.com>
2022-03-15 14:28:43 +0300

    (feat) upd generation

--- backend/tests/data/index.go
+++ backend/tests/data/index.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
+	"math/rand"
+	"strings"
 	"time"
 
 	"rainbow-umbrella/internal/infrastruct"
 	"rainbow-umbrella/internal/objects/bo"
 	"rainbow-umbrella/internal/objects/dao"
 	"rainbow-umbrella/internal/repos"
+)
+
+var (
+	Cities = []string{
+		"Paris", "Moscow", "New York", "London", "LosAngeles",
+		"Berlin", "Piter", "Roma", "Oslo", "Amsterdam",
+	}
 )
 
 const UserCount = 3
@@ -26,7 +36,10 @@ func main() {
 
 	userRepo := repos.NewUserRepo(dbClient)
 
-	userGeneratorInst := new(userGenerator).build()
+	userGeneratorInst, err := new(userGenerator).build()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	for i := 0; i < UserCount; i++ {
 		user := userGeneratorInst.generateUserDAO()
@@ -39,19 +52,66 @@ func main() {
 
 type userGenerator struct {
 	loginCounter   int64
+	loginPrefix    string
 	hashedPassword string
+
+	firstNames []string
+	lastNames  []string
 }
 
-func (g *userGenerator) build() *userGenerator {
+func (g *userGenerator) build() (*userGenerator, error) {
+	rand.Seed(time.Now().Unix())
+
 	g.loginCounter = 0
 	g.hashedPassword = "$2a$10$F.QZ5yG1OVLeSjb.kNGKveVAt6oIT8IYi5bavieoel3A5PHDJlFNG"
+	g.loginPrefix = fmt.Sprintf("gen_login_%v", time.Now().Unix())
 
-	return g
+	firstNamesRaw, err := ioutil.ReadFile("first_names.txt")
+	if err != nil {
+		return nil, err
+	}
+	for _, firstName := range strings.Split(string(firstNamesRaw), "\n") {
+		if value := strings.TrimSpace(firstName); value != "" {
+			g.firstNames = append(g.firstNames, value)
+		}
+	}
+
+	lastNamesRaw, err := ioutil.ReadFile("last_names.txt")
+	if err != nil {
+		return nil, err
+	}
+	for _, lastName := range strings.Split(string(lastNamesRaw), "\n") {
+		if value := strings.TrimSpace(lastName); value != "" {
+			g.lastNames = append(g.lastNames, value)
+		}
+	}
+
+	return g, nil
 }
 
 func (g *userGenerator) generateLogin() string {
 	g.loginCounter++
-	return fmt.Sprintf("gen_login_%v", g.loginCounter)
+	return fmt.Sprintf("%v_%v", g.loginPrefix, g.loginCounter)
+}
+
+func (g *userGenerator) generateCity() string {
+	return Cities[rand.Intn(len(Cities))]
+}
+
+func (g *userGenerator) generateFirstName() string {
+	return g.firstNames[rand.Intn(len(g.firstNames))]
+}
+
+func (g *userGenerator) generateLastName() string {
+	return g.lastNames[rand.Intn(len(g.lastNames))]
+}
+
+func (g *userGenerator) generateGender() string {
+	probability := rand.Float64()
+	if probability >= 0.51 {
+		return "Female"
+	}
+	return "Male"
 }
 
 func (g *userGenerator) generateUserDAO() *dao.User {
@@ -60,6 +120,10 @@ func (g *userGenerator) generateUserDAO() *dao.User {
 	userBO.Login = g.generateLogin()
 	userBO.HashedPassword = g.hashedPassword
 	userBO.CreatedAt = time.Now()
+	userBO.City = g.generateCity()
+	userBO.FirstName = g.generateFirstName()
+	userBO.LastName = g.generateLastName()
+	userBO.Gender = g.generateGender()
 
 	return new(dao.User).FromBO(userBO)
 }
