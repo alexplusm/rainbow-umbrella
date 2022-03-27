653004c09ec16d72426edde96ecfe6b83fc8afc6
dark.qvarkq <dark.qvark@gmail.com>
2022-03-27 18:20:51 +0300

    (feat) upd schema.sql

--- backend/scripts/schema.sql
+++ backend/scripts/schema.sql
@@ -1,17 +1,17 @@
-CREATE TABLE IF NOT EXISTS users (
-    user_id           SERIAL,
-    login             VARCHAR(32)   NOT NULL UNIQUE,
-    hashed_password   VARCHAR(255)  NOT NULL,
+CREATE TABLE IF NOT EXISTS `users` (
+    `user_id`           SERIAL,
+    `login`             VARCHAR(32)   NOT NULL UNIQUE,
+    `hashed_password`   VARCHAR(255)  NOT NULL,
 
-    first_name        VARCHAR(255)  NOT NULL,
-    last_name         VARCHAR(255)  NOT NULL,
-    birthday          DATE          NOT NULL,
-    gender            VARCHAR(255)  NOT NULL,
-    city              VARCHAR(255)  NOT NULL,
+    `first_name`        VARCHAR(255)  NOT NULL,
+    `last_name`         VARCHAR(255)  NOT NULL,
+    `birthday`          DATE          NOT NULL,
+    `gender`            VARCHAR(255)  NOT NULL,
+    `city`              VARCHAR(255)  NOT NULL,
 
-    created_at        TIMESTAMP     NOT NULL,
+    `created_at`        TIMESTAMP     NOT NULL,
 
-    PRIMARY KEY (user_id)
+    PRIMARY KEY (`user_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `friendships` (
@@ -23,12 +23,28 @@ CREATE TABLE IF NOT EXISTS `friendships` (
     `created_at`        TIMESTAMP     NOT NULL,
     `updated_at`        TIMESTAMP,
 
-    PRIMARY KEY (`friendship_id`),
-
     CONSTRAINT `unique_users` UNIQUE(`requesting_user_id`, `targeting_user_id`),
 
     FOREIGN KEY (`requesting_user_id`)    REFERENCES `users`(`user_id`),
-    FOREIGN KEY (`targeting_user_id`)     REFERENCES `users`(`user_id`)
+    FOREIGN KEY (`targeting_user_id`)     REFERENCES `users`(`user_id`),
+
+    PRIMARY KEY (`friendship_id`)
 );
 
---      "interests"             ... MANY TO MANY ?
+CREATE TABLE IF NOT EXISTS `user_interests` (
+    `user_interest_id`  SERIAL,
+    `user_id`           BIGINT UNSIGNED   NOT NULL,
+    `interest_id`       BIGINT UNSIGNED   NOT NULL,
+
+    FOREIGN KEY (`user_id`)         REFERENCES `users`(`user_id`),
+    FOREIGN KEY (`interest_id`)     REFERENCES `interests`(`interest_id`),
+
+    PRIMARY KEY (`user_interest_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `interests` (
+    `interest_id`   SERIAL,
+    `value`         VARCHAR NOT NULL,
+
+    PRIMARY KEY (`interest_id`)
+);
